0d8371048f0cc5fdd339d46be794b69ad46a496c
Jason de Wolff <76750361+JasondeWolff@users.noreply.github.com>
2025-02-11 15:03:27 +0100

    build with clang

--- build.rs
+++ build.rs
@@ -2,41 +2,39 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
+    std::env::set_var("CC", "C:\\Program Files\\LLVM\\bin\\clang.exe");
+
     // Compile and link to the c++ binaries
     #[cfg(feature = "simd")]
     cc::Build::new()
         .file("ffi/wrapper.cpp")
         .flag("-mavx")
-        .flag("-Ofast")
-        //.flag("-O3")
-        //.flag("-ffast-math")
-        //.flag_if_supported("/arch:AVX")
-        //.flag_if_supported("/O2")
-        //.flag_if_supported("/Oi")
-        //.flag_if_supported("/Ot")
-        .compiler("C:\\mingw64\\bin\\gcc.exe")
-        //.compiler("C:\\Program Files\\LLVM\\bin\\clang.exe")
+        .flag("-O3")
+        .flag("-ffast-math")
         .compile("tinybvh");
     #[cfg(not(feature = "simd"))]
     cc::Build::new()
         .file("ffi/wrapper.cpp")
         .flag("-D TINYBVH_NO_SIMD")
+        .flag("-O3")
+        .flag("-ffast-math")
         .compile("tinybvh");
 
     println!("cargo:rustc-link-search={}", env::var("OUT_DIR").unwrap());
     println!("cargo:rustc-link-lib=tinybvh");
 
-    //#[cfg(feature = "simd")]
-    //std::env::set_var("BINDGEN_EXTRA_CLANG_ARGS", "-mavx -O3");
+    #[cfg(feature = "simd")]
+    std::env::set_var("BINDGEN_EXTRA_CLANG_ARGS", "-mavx -O3 -ffast-math");
     #[cfg(not(feature = "simd"))]
-    std::env::set_var("BINDGEN_EXTRA_CLANG_ARGS", "-D TINYBVH_NO_SIMD");
+    std::env::set_var(
+        "BINDGEN_EXTRA_CLANG_ARGS",
+        "-D TINYBVH_NO_SIMD -O3 -ffast-math",
+    );
 
     // Generate rust bindings
     let bindings = bindgen::Builder::default()
         .header("ffi/wrapper.hpp")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
-        .blocklist_item("__gnu_cxx::__min")
-        .blocklist_item("__gnu_cxx::__max")
         .generate()
         .expect("Unable to generate bindings");
 
